edf1e6e4de520f6bf3ac2a0e3dea03a47259c473
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-06-15 17:44:55 +0200

    chore: update to cdktf 0.17 (#13)

---
+++ scripts/update-cdktf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
+CDKTF_VERSION=$1
+
+echo "Updating to cdktf version $CDKTF_VERSION"
+cd $PROJECT_ROOT
+
+yarn add -D -W cdktf-cli@$CDKTF_VERSION
+yarn add -W cdktf@$CDKTF_VERSION
