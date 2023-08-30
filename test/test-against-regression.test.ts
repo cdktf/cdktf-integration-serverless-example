6412d62c74a3faecfe77fc8b403694312028adb2
Mark DeCrane <mark.decrane@hashicorp.com>
2023-08-30 14:59:51 -0400

    chore: add dependabot, auto-merge, auto-approve, and regression test workflows

---
+++ test/test-against-regression.test.ts
@@ -0,0 +1,27 @@
+import { FrontendStack } from "../main";
+import { PostsStack } from "../main";
+import { Testing } from "cdktf";
+
+describe("Tests against regression of synth output", () => {
+    it("has no change in FrontendStack Configuration", () => {
+        const app = Testing.app();
+        const stack = new FrontendStack(app, "test-frontend", {
+            environment: "development",
+            user: "regression-test",
+            apiEndpoint: "N/A",
+        });
+        const stackSynth = JSON.parse(Testing.synth(stack));
+        delete stackSynth["terraform"]
+        expect(JSON.stringify(stackSynth, null, 2)).toMatchSnapshot();
+    });
+    it("has no change in PostsStack Configuration", () => {
+        const app = Testing.app();
+        const stack = new PostsStack(app, "test-posts", {
+            environment: "development",
+            user: "regression-test",
+        });
+        const stackSynth = JSON.parse(Testing.synth(stack));
+        delete stackSynth["terraform"]
+        expect(JSON.stringify(stackSynth, null, 2)).toMatchSnapshot();
+    });
+});
